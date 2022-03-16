0aa1d944e21e472d6d87c8572a7d2a252fe94cd7
Rishabh Kumar <rishabh.kumar@outlook.com>
2022-03-16 15:28:49 +0200

    material_verification

--- lib/interface/job_item_interface/details/job_item_details_page.dart
+++ lib/interface/job_item_interface/details/job_item_details_page.dart
@@ -22,7 +22,9 @@ class JobItemDetailsPage extends StatefulWidget {
 }
 
 class _JobItemDetailsPageState extends State<JobItemDetailsPage> {
-  double currentWeight = 170;
+  double currentWeight = 200.5;
+  bool isVerified = false;
+  bool isMaterialScanned = false;
   String back = '{"action":"back"}';
   String tare = '{"action":"tare"}';
   String complete = '{"action":"complete"}';
@@ -40,66 +42,81 @@ class _JobItemDetailsPageState extends State<JobItemDetailsPage> {
   }
 
   dynamic listenToScanner(String data) {
-    Map<String, dynamic> scannerData = jsonDecode(data
-        .replaceAll(";", ":")
-        .replaceAll("[", "{")
-        .replaceAll("]", "}")
-        .replaceAll("'", "\"")
-        .replaceAll("-", "_"));
-    switch (scannerData["action"]) {
-      case "back":
-        navigationService.pushReplacement(
-          CupertinoPageRoute(
-            builder: (BuildContext context) => JobDetailsPage(
-              jobID: widget.jobID,
+    if (!data.contains("[") || !data.contains("]")) {
+      verifyMaterial(data);
+    } else {
+      Map<String, dynamic> scannerData = jsonDecode(data
+          .replaceAll(";", ":")
+          .replaceAll("[", "{")
+          .replaceAll("]", "}")
+          .replaceAll("'", "\"")
+          .replaceAll("-", "_"));
+      switch (scannerData["action"]) {
+        case "back":
+          navigationService.pushReplacement(
+            CupertinoPageRoute(
+              builder: (BuildContext context) => JobDetailsPage(
+                jobID: widget.jobID,
+              ),
             ),
-          ),
-        );
-        break;
-      case "complete":
-        //TODO post to backend and go back to previous page if post is successful.
-        break;
-      case "tare":
-        setState(() {
-          currentWeight = 0;
-        });
-        break;
-      default:
+          );
+          break;
+        case "complete":
+          //TODO post to backend and go back to previous page if post is successful.
+          break;
+        case "tare":
+          setState(() {
+            currentWeight = 0;
+          });
+          break;
+        default:
+      }
+    }
+  }
+
+  Future<dynamic> verifyMaterial(String scannerData) async {
+    String itemBarcode = widget.jobItem["barcode"];
+    if (!isMaterialScanned) {
+      setState(() {
+        isMaterialScanned = true;
+      });
+    }
+    if (itemBarcode == scannerData) {
+      setState(() {
+        isVerified = true;
+      });
     }
   }
 
   List<Widget> getRowWidget(Map<String, dynamic> jobItem) {
     List<Widget> widgets = [];
     jobItem.forEach((key, value) {
-      if (key != "complete") {
-        widgets.add(
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                key.toUpperCase().replaceAll("_", " "),
-                style: const TextStyle(
-                  fontSize: 18.0,
-                ),
+      widgets.add(
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              key.toUpperCase().replaceAll("_", " "),
+              style: const TextStyle(
+                fontSize: 18.0,
               ),
-              Text(
-                value.toString().toUpperCase(),
-                style: const TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                ),
+            ),
+            Text(
+              value.toString().toUpperCase(),
+              style: const TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
               ),
-            ],
-          ),
-        );
-      }
+            ),
+          ],
+        ),
+      );
     });
     return widgets;
   }
 
-  @override
-  Widget build(BuildContext context) {
+  Widget verifiedState() {
     Map<String, dynamic> jobItem = widget.jobItem;
     double upperLimit = double.parse(jobItem["upper_limit"]);
     double requiredQty = double.parse(jobItem["required"]);
@@ -111,168 +128,189 @@ class _JobItemDetailsPageState extends State<JobItemDetailsPage> {
             : (upperLimit - requiredQty) >= 0.1
                 ? 2
                 : 3;
-    return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: 100.0,
-            padding: const EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 0.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: getRowWidget(jobItem),
-            ),
+    return Column(
+      children: [
+        Container(
+          height: 100.0,
+          padding: const EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 0.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: getRowWidget(jobItem),
           ),
-          const Divider(
-            height: 20.0,
-          ),
-          Expanded(
-            child: BaseWidget(
-              builder: (context, sizeInformation) {
-                return SizedBox(
-                  height: sizeInformation.screenSize.height - 120,
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        width: sizeInformation.screenSize.width * 0.6,
-                        height: sizeInformation.screenSize.height - 120,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Text(
-                                  currentWeight.toStringAsFixed(precision),
-                                  style: TextStyle(
-                                      fontSize: 300.0 *
-                                          sizeInformation.screenSize.height /
-                                          1006,
-                                      color: (currentWeight > upperLimit ||
-                                              currentWeight < lowerLimit)
-                                          ? Colors.red
-                                          : Colors.green),
-                                ),
-                                Icon(
-                                  currentWeight < lowerLimit
-                                      ? Icons.arrow_circle_up
-                                      : currentWeight > upperLimit
-                                          ? Icons.arrow_circle_down
-                                          : Icons.note_outlined,
-                                  size: 200.0 *
-                                      sizeInformation.screenSize.height /
-                                      1006,
-                                  color: (currentWeight < lowerLimit ||
-                                          currentWeight > upperLimit)
-                                      ? Colors.red
-                                      : Colors.transparent,
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
+        ),
+        const Divider(
+          height: 20.0,
+        ),
+        Expanded(
+          child: BaseWidget(
+            builder: (context, sizeInformation) {
+              return SizedBox(
+                height: sizeInformation.screenSize.height - 120,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: sizeInformation.screenSize.width * 0.6,
+                      height: sizeInformation.screenSize.height - 120,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                currentWeight.toStringAsFixed(precision),
+                                style: TextStyle(
+                                    fontSize: 300.0 *
+                                        sizeInformation.screenSize.height /
+                                        1006,
+                                    color: (currentWeight > upperLimit ||
+                                            currentWeight < lowerLimit)
+                                        ? Colors.red
+                                        : Colors.green),
+                              ),
+                              Icon(
+                                currentWeight < lowerLimit
+                                    ? Icons.arrow_circle_up
+                                    : currentWeight > upperLimit
+                                        ? Icons.arrow_circle_down
+                                        : Icons.check_circle,
+                                size: 200.0 *
+                                    sizeInformation.screenSize.height /
+                                    1006,
+                                color: (currentWeight < lowerLimit ||
+                                        currentWeight > upperLimit)
+                                    ? Colors.red
+                                    : Colors.green,
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
-                      SizedBox(
-                        width: sizeInformation.screenSize.width * 0.4,
-                        height: sizeInformation.screenSize.height - 120,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SizedBox(
-                                  width: sizeInformation.screenSize.width * 0.2,
-                                  child: Center(
-                                    child: QrImage(
-                                      data: tare,
-                                      size: 200.0,
+                    ),
+                    SizedBox(
+                      width: sizeInformation.screenSize.width * 0.4,
+                      height: sizeInformation.screenSize.height - 120,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                width: sizeInformation.screenSize.width * 0.2,
+                                child: Center(
+                                  child: QrImage(
+                                    data: tare,
+                                    size: 200.0,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: sizeInformation.screenSize.width * 0.2,
+                                child: const Center(
+                                  child: Text(
+                                    "Tare",
+                                    style: TextStyle(
+                                      fontSize: 30.0,
+                                      fontWeight: FontWeight.bold,
                                     ),
                                   ),
                                 ),
-                                SizedBox(
-                                  width: sizeInformation.screenSize.width * 0.2,
-                                  child: const Center(
-                                    child: Text(
-                                      "Tare",
-                                      style: TextStyle(
-                                        fontSize: 30.0,
-                                        fontWeight: FontWeight.bold,
-                                      ),
+                              ),
+                            ],
+                          ),
+                          const Divider(
+                            color: Colors.white,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                width: sizeInformation.screenSize.width * 0.2,
+                                child: Center(
+                                  child: QrImage(
+                                    data: "Complete",
+                                    size: 200.0,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: sizeInformation.screenSize.width * 0.2,
+                                child: const Center(
+                                  child: Text(
+                                    "Complete",
+                                    style: TextStyle(
+                                      fontSize: 30.0,
+                                      fontWeight: FontWeight.bold,
                                     ),
                                   ),
                                 ),
-                              ],
-                            ),
-                            const Divider(
-                              color: Colors.white,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SizedBox(
-                                  width: sizeInformation.screenSize.width * 0.2,
-                                  child: Center(
-                                    child: QrImage(
-                                      data: "Complete",
-                                      size: 200.0,
+                              ),
+                            ],
+                          ),
+                          const Divider(
+                            color: Colors.white,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                width: sizeInformation.screenSize.width * 0.2,
+                                child: Center(
+                                  child: QrImage(
+                                    data: back,
+                                    size: 200.0,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: sizeInformation.screenSize.width * 0.2,
+                                child: const Center(
+                                  child: Text(
+                                    "Back",
+                                    style: TextStyle(
+                                      fontSize: 30.0,
+                                      fontWeight: FontWeight.bold,
                                     ),
                                   ),
                                 ),
-                                SizedBox(
-                                  width: sizeInformation.screenSize.width * 0.2,
-                                  child: const Center(
-                                    child: Text(
-                                      "Complete",
-                                      style: TextStyle(
-                                        fontSize: 30.0,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            const Divider(
-                              color: Colors.white,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SizedBox(
-                                  width: sizeInformation.screenSize.width * 0.2,
-                                  child: Center(
-                                    child: QrImage(
-                                      data: back,
-                                      size: 200.0,
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: sizeInformation.screenSize.width * 0.2,
-                                  child: const Center(
-                                    child: Text(
-                                      "Back",
-                                      style: TextStyle(
-                                        fontSize: 30.0,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                );
-              },
-            ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
           ),
-        ],
-      ),
+        ),
+      ],
+    );
+  }
+
+  Widget unScannedState() {
+    return const Center(
+      child: Text("Please Scan Material Barcode."),
+    );
+  }
+
+  Widget unVerifiedState() {
+    return const Center(
+      child: Text("Incorrect Material. Please Scan Correct Material."),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: isMaterialScanned
+          ? isVerified
+              ? verifiedState()
+              : unVerifiedState()
+          : unScannedState(),
     );
   }
 }
